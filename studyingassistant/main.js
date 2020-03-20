0da9fbc3b09468cda3593eef761ad0ac20f94398
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-03-20 00:39:59 -0400

    changed back to ES5 due to error

--- studyingassistant/main.js
+++ studyingassistant/main.js
@@ -1,6 +1,6 @@
-import electron from 'electron';
-import { format } from 'url';
-import { join } from 'path';
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
 
 const {app, BrowserWindow, Menu} = electron;
 
@@ -11,8 +11,8 @@ app.on('ready', function(){
     // Create new window
     mainWindow = new BrowserWindow({width:800, height:600});
     // Load html into window
-    mainWindow.loadURL(format({
-        pathname: join(__dirname, 'mainWindow.html'),
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:', 
         slashes: true
     }));
