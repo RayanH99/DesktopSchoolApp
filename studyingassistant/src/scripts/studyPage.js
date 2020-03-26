fce87bd35e873e1cf01d08d6c15b579743c5c09a
rayanh99 <rayanh_99@yahoo.com>
2020-03-26 04:42:35 -0400

    added functionality to start, stop, and reset buttons for timer

--- studyingassistant/src/scripts/studyPage.js
+++ studyingassistant/src/scripts/studyPage.js
@@ -1,9 +1,35 @@
-//var pomodoroTimer = document.getElementById('pomodoro-timer');
-var setTime = 25;
-var timer = setTime;
+var startTime = 25; // this value will change depending on what the user selects, default value is 25:00
+var time = startTime * 60; //number of seconds total
+var intervalID; //used for setInterval()
 
-for(var i=0; i<setTime; i++){
-    timer -= 1;
-    //    pomodoroTimer.innerHTML = timer;
+var pomodoroTimer = document.getElementById('pomodoro-timer');
+
+
+function updateTimer(){
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds; //if seconds less than 10, make it 00, 01, 02, etc.
+
+    pomodoroTimer.innerHTML = minutes + ':'+ seconds;
+    time--;
 }
 
+function startTimer(){
+    if(!intervalID){ // to prevent multiple setIntervals being queued up
+        updateTimer(); // call this once to stop clock from taking too long on first use of the setInterval function
+        intervalID = setInterval(updateTimer, 1000);
+    }
+}
+
+function stopTimer(){
+    clearInterval(intervalID);
+    intervalID = 0;
+}
+
+function resetTimer(){
+    time = startTime * 60;
+    updateTimer();
+    stopTimer();
+}
