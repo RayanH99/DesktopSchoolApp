2fd838034fb19d8b5c5250f36dd72b494c56304b
Rayan <rayanh_99@yahoo.com>
2020-03-25 21:49:17 -0400

    added buttons/timer for study page, added js file for studypage

---
+++ studyingassistant/src/scripts/studyPage.js
@@ -0,0 +1,9 @@
+//var pomodoroTimer = document.getElementById('pomodoro-timer');
+var setTime = 25;
+var timer = setTime;
+
+for(var i=0; i<setTime; i++){
+    timer -= 1;
+    //    pomodoroTimer.innerHTML = timer;
+}
+
