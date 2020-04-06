8543491c2cb3daae66fadab0b358fd11e5484044
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-04-05 23:19:15 -0400

    completed authentication for signup, login, and signout

--- studyingassistant/src/scripts/welcomeMessage.js
+++ studyingassistant/src/scripts/welcomeMessage.js
@@ -1,3 +1,5 @@
+require('../scripts/dbConnection');
+
 var myDate = new Date();
 var hrs = myDate.getHours();
 var greet;
@@ -11,3 +13,34 @@ else if (hrs >= 17 && hrs <= 24)
     greet = 'Good Evening';
 
 welcomeMsg.innerHTML = greet + ', Corona! 👋';
+
+var logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener('click', function()
+{
+    firebase.auth().signOut()
+    .then(function() 
+    {
+        // Sign-out successful.
+        document.location.href = "../pages/loginPage.html";
+    })
+    .catch(function(error) 
+    {
+        // An error happened
+        if(error)
+        {
+            notifier.notify(
+                {
+                  title: 'The Study App',
+                  message: error.message,
+                  // icon: path.join(__dirname, 'coulson.jpg'),
+                  sound: true, // Only Notification Center or Windows Toasters
+                  wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait
+                },
+                function(err, response) {
+                  // Response is response from notification
+                }
+            );
+        }
+    });
+})
