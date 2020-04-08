26a620820c40e27978c939a7a8bf7a82856be9a3
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-04-08 18:43:06 -0400

    added Main and Renderer processes for frontend/backend communication

--- studyingassistant/src/scripts/loginPage.js
+++ studyingassistant/src/scripts/loginPage.js
@@ -1,4 +1,6 @@
 require('../scripts/dbConnection');
+var db = firebase.firestore();
+const {ipcRenderer} = require('electron');
 
 //notifications
 const notifier = require('node-notifier');
@@ -12,25 +14,39 @@ loginBtn.addEventListener('click', function()
     var emailField = document.getElementById("inputEmail3").value;
     var passField = document.getElementById("inputPassword3").value;
 
-    firebase.auth().signInWithEmailAndPassword(emailField, passField)
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
     .then(function(){
-        document.location.href = "../pages/mainWindow.html";
-    })
-    .catch(function(error) {
-        if(error)
+        return firebase.auth().signInWithEmailAndPassword(emailField, passField)
+        .then(function()
         {
-            notifier.notify(
-                {
-                  title: 'The Study App',
-                  message: error.message,
-                  // icon: path.join(__dirname, 'coulson.jpg'),
-                  sound: true, // Only Notification Center or Windows Toasters
-                  wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait
-                },
-                function(err, response) {
-                  // Response is response from notification
-                }
-            );
-        }
+            var user = firebase.auth().currentUser;
+
+            //send info to backend
+            ipcRenderer.send('sendUserInfo', user);
+
+            return window.location = "../pages/mainWindow.html";
+        })
+        .catch(function(error) 
+        {
+            if(error)
+            {
+                notifier.notify(
+                    {
+                      title: 'The Study App',
+                      message: error.message,
+                      // icon: path.join(__dirname, 'coulson.jpg'),
+                      sound: true, // Only Notification Center or Windows Toasters
+                      wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait
+                    },
+                    function(err, response) {
+                      // Response is response from notification
+                    }
+                );
+            }
+        });
+    })
+    .catch(function(error)
+    {
+        console.log(error);
     });
 });
