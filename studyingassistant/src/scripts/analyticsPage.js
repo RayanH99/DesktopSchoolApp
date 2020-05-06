90c0460cdad4ee7eaa300ab8f57576a785bf56ea
Uzair Jawaid <uzair-jawaid@hotmail.com>
2020-05-06 19:45:22 -0400

    restyled analytics graphs and calculated average values

--- studyingassistant/src/scripts/analyticsPage.js
+++ studyingassistant/src/scripts/analyticsPage.js
@@ -29,42 +29,57 @@ ipcRenderer.invoke('getUser')
     console.log("Error getting document:", error);
 });
 
+studiedHours = [3.5, 4, 2.5, 5, 0.5, 6, 3.25];
+let avgStudy = studiedHours.reduce((a, b) => a + b, 0) / studiedHours.length;
+let avgBreak = 0;
+
+document.getElementById("avgStudy").innerText = 'Average Time Studied: '+avgStudy.toFixed(2)+' hours';
+document.getElementById("avgBreak").innerText = 'Average Break Time: '+avgBreak.toFixed(2)+' hours';
+
 var ctx = document.getElementById('chart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
         datasets: [{
-            label: 'Hours Studied',
-            data: [3.5, 4, 2.5, 5, 0, 6, 3.25],
+            label: 'hours studied',
+            data: studiedHours,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(204, 64, 255,0.2)'
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.7)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(204, 64, 255,1)'
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+        legend: {
+            display: false
+        },
         scales: {
             yAxes: [{
+                display: true,
                 ticks: {
                     beginAtZero: true
                 }
             }]
+        },
+        title: {
+            display: true,
+            text: 'Hours Studied'
         }
     }
 });
